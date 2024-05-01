203713b5efdc8bf20307d3f2cc607695c116a4c2
MessiasBRSPRO <meddisme@gmail.com>
2024-05-01 16:25:07 -0300

    Refatoring the withdrawMethod

--- src/br/com/ProjectOne/AccountsType/ContaBancaria.java
+++ src/br/com/ProjectOne/AccountsType/ContaBancaria.java
@@ -14,20 +14,8 @@ public class ContaBancaria extends Conta{
 
     @Override // this decorator signalize the modification of existent method in mother-Class
     public void withdrawal(double value) {
-        CRUDConta crudConta = new CRUDConta();
-        double applyTaxes = value + (value * tax);
-        this.actualBalance -= applyTaxes;
-        boolean balanceValidation = actualBalance < 0;
-        try{
-            if(balanceValidation){
-                //If the withdrawal value is more bigger of your balance, you will be enter in debit
-                throw new NegativeBalanceException("your account is in debit, have a negative balance");
-            }
-            operationsExtract.add("WithDrawal $"+value + "+ tax:$"+(value * tax));
-            crudConta.update(this);
-        }catch (NegativeBalanceException e){
-            System.out.println("an Exception has occurred:"+e.getMessage());
-        }
+        double valueTaxed = value + (value * tax);
+        super.withdrawal(valueTaxed);
     }
 
 
